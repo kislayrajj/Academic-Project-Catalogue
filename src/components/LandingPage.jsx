da0738f7cfdd82cba9d78ba219d1cbab1e468fc7
kislayrajj <wrkislayraj@gmail.com>
2024-12-06 23:01:09 +0530

    basic structuring

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -4,8 +4,11 @@ const LandingPage = () => {
   return (
     <div>
       <div className="h-screen flex justify-center items-center bg-gradient-to-r from-blue-200 to-cyan-200">
-        <h1 className="text-xl xl:text-5xl font-semibold xl:font-bold h-24 bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent"> Academic Project Catalogue !</h1>
-        
+        <h1 className="text-xl xl:text-5xl font-semibold xl:font-bold h-24 bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent">
+          {" "}
+          Academic Project Catalogue !
+        </h1>
+        <div className="text-blue-400">(Level - one)</div>
       </div>
     </div>
   );
